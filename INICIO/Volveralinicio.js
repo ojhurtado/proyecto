397994d9ce0a8070c15106fea51ed46d774746dd
JOSÉ MOISÉS NÚÑEZ SILVA <moisesdogor@hotmail.com>
2025-11-27 18:04:11 -0500

    Add files via upload

---
+++ INICIO/Volveralinicio.js
@@ -0,0 +1,7 @@
+// volver hacia rraba de la pagina
+function backGoToUp() {
+  document.body.scrollTop = document.documentElement.scrollTop = 0;
+}
+function redirec(){
+  window.location="../Calculadora/Calculadora.html"
+}
