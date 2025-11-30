c30dda2c1706acab758c20891b677338641333ae
jose moises nuñez <moisesdogor@hotmail.com>
2025-11-30 12:19:22 -0500

    NUEVAS ACTUALIZACIONES

--- calculadora/calculadora.js
+++ calculadora/calculadora.js
@@ -1,23 +1,27 @@
+function proceso(){
+    
+}
+
 function resultado(){
 
     const valor = document.getElementById("kwh").value;
 
-    const coversion= parseFloat(valor)
+    const cov= parseFloat(valor)
 
-    const twh= convierte_twh(coversion)
+    const twh= convierte_twh(cov)
 
     const porcent=58.19
 
     const porcentaje= ((twh*100)/porcent).toFixed(4)
 
-    const resultado2="Tu consumo promedio mensual fue de "+valor+"kwh, lo cual es quivalente al "+ porcentaje+"% a la totalidad consumida de la fuente solar producida en Colombia con respecto al año 2021";
+    const res="Su consumo promedio mensual fue de "+valor+"kwh, lo cual es quivalente al "+ porcentaje+"% a la totalidad connsumida de la fuente hidrica producida en el pais Colombiano con respecto al año 2021";
 
 
     if(valor != ""){
 
         Swal.fire({
-            title: 'Buen trabajo',
-            text: resultado2,
+            title: 'excelente',
+            text: res,
             icon: 'success',
             confirmButtonText: 'Cool'
         })
@@ -40,6 +44,12 @@ function resultado(){
 function convierte_twh(dato){
 
     return (dato/1000)*12
+
+}
+
+function limpia(){
+
+    document.getElementById("kwh").value=""
 
 }
 
